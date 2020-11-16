bf534925f2dc336dd376cb93d1c879163f673404
Jedidiah <hello@jedidiah.dev>
2020-11-17 00:58:08 +0100

    Add stubs for Slices

---
+++ slices/NowPlaying/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+// import { RichText } from "prismic-reactjs";
+
+const NowPlaying = ({ slice }) => (
+  <section>
+    <h2>Now Playing</h2>
+  </section>
+);
+
+export default NowPlaying;
--- slices/index.js
+++ slices/index.js
@@ -1,1 +1,5 @@
 export { default as NowPlaying } from "./NowPlaying";
+
+export { default as EpisodeList } from "./EpisodeList";
+export { default as EpisodeNotes } from "./EpisodeNotes";
+export { default as AudioPlayer } from "./AudioPlayer";
